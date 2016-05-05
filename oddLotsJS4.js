88da1c755fad190189f763b79b323f79b611fde4
Sara E <eichnersara@gmail.com>
2016-05-05 12:57:14 -0400

    update js for odd lots

--- oddLotsJS4.js
+++ oddLotsJS4.js
@@ -1,34 +1,12 @@
-var map = L.map('map').setView([40.71,-73.93], 12);
 
-// set a tile layer to be CartoDB tiles
+var map = L.map('map').setView([40.71,-73.93], 12);
 
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',{
 attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://www.openstreetmap.org/copyright">ODbL</a>.',
 maxZoom: 19
 }).addTo(map);
 
-// two datasets, both geojson:
-// should I leave parameters empty and load at end? if so, how is that var defined?
-// I can nadd data name here (NYBBPolygon) or at start of function which would be defined here 
-// and framed around function
-
-
-//var NYBBgeoJSON;
-//NYBBgeoJSON.addTo(map);
-
-var datalayer = L.geoJson(null, {
-    style: lotStyle,
-    onEachFeature: lotClick
-});//.addTo(map);
-
-// STYLES FOR NYBB POLYGON
-addNYBB(); 
-
-function addNYBB() {
-$.getJSON( "geojson/nybb.geojson", function( data ) {
-    var NYBBPolygon = data; 
-//console.log('anything');
-    var NYBBstyle = function (feature) {
+NYBBstyle = function (feature) {
         var value = feature.properties.BoroCode;
         if(value === 3){
             fillColor = "#3f3f3f";
@@ -63,21 +41,13 @@ $.getJSON( "geojson/nybb.geojson", function( data ) {
         };
         return style;
     }
- //   console.log(data);
-
-    NYBBgeoJSON = L.geoJson(NYBBPolygon, {
-        style: NYBBstyle
-    }).addTo(map);
-
-    //addOddLotsData();
-});
-}
+    console.log('is it adding polygon layer?');
 
 
-// STYLES FOR LOT DATA/ DATALAYER
+var lotStyle;
+var lotClick;
 
-//lotStyle = pointToLayer(function (feature, layer){
-var lotStyle = function (feature, dataLayer){
+lotStyle = function (feature){
 
     var value = feature.properties.lottype;
     if(value === '1'){
@@ -85,20 +55,20 @@ var lotStyle = function (feature, dataLayer){
         color = "#fee5d9";
     }
     if(value === '6'){
-        fillColor = "#5CB3FF";
-        color = '#5CB3FF';
+        fillColor = "#A6D5FF";
+        color = '#A6D5FF';
     }
     if(value === '7'){
-        fillColor = "#00B2B2";
-        color = '#00B2B2';
+        fillColor = "#40C9C9";
+        color = '#40C9C9';
     }
     if(value === '9'){
-        fillColor = "#FA550F";
-        color = '#FA550F';
+        fillColor = "#FC923A";
+        color = '#FC923A';
     }
     if(value === '8') { 
-        fillColor = "#46a346";
-        color = '#46a346';
+        fillColor = "#FF8563";
+        color = '#FF8563';
     }
 
     var style = {
@@ -108,62 +78,80 @@ var lotStyle = function (feature, dataLayer){
         fillOpacity: 0.9,
         fillColor: fillColor
     };
+
     //console.log(feature);
     return style;
-},
+}
 
-// ON EACH FEATURE FUNCTION
-lotClick = function (feature, layer) {
-    layer.bindPopup("This odd lot address is " + 
+
+//LOT CLICK BINDPOPUP
+lotClick = function (feature, dataLayer) {
+    dataLayer.bindPopup("This odd lot address is " + 
         "<strong>" + feature.properties.address + "</strong>" + 
         " and it is owned by " + 
         "<strong>" + feature.properties.ownername + "</strong>");
 }
 
-        console.log('this will never work');
-// CHANGE FUNCTION
-function addDataToMap(dataLayer, choice) {
-    $('.choice').change(function () {
-        console.log('this will never work');
-        var sql = 'SELECT * FROM bk_oddlots';
-        if ($(this).val() === 'lottype') {
-        }
-        else {
-            sql += " WHERE lottype = '" + ($(this).val() + "'" );
-        }
-        console.log(sql);
+var NYBBgeoJSON = L.geoJson(null, {
+        style: NYBBstyle     
+}).addTo(map);
 
-        var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
-            q: sql,
-            format: 'GeoJSON'        
-        });
-        $.getJSON(url)
+var dataLayer = L.geoJson(null, {  
+        style: lotStyle,
+        onEachFeature: lotClick,
+}).addTo(map);
 
-        .done(function (data) {
-        //console.log(data);
+var Choice;
+var NYBBstyle
+
+$.getJSON( "geojson/nybb.geojson", function( data ) {
+    var NYBBpolygon = data;   
+    NYBBgeoJSON.addData(data);
+    NYBBgeoJSON.bringToBack();
+});
+
+
+/// CHOICE/OPTION FUNCTION 
+Choice = function addDataToMap(dataLayer, feature) {
+// log out parameters
+//$('.choice').change(function () {
+    console.log('something');
+    var sql = 'SELECT * FROM bk_oddlots_copy';
+    console.log(feature);
+    if (feature === 'lottype') {
+    }
+    else {
+        sql += " WHERE lottype = '" + feature + "'";
+    }
+    console.log(sql);
+
+    var url = 'https://eichnersara.cartodb.com/api/v2/sql?' + $.param({
+        q: sql,
+        format: 'GeoJSON'        
+    });
+   
+    $.getJSON(url).done(function (data) {
+        console.log(data);
         dataLayer.clearLayers();
         dataLayer.addData(data);
-
-        });
-
+        map.fitBounds(dataLayer.getBounds());
+        dataLayer.bringToFront();
     });
-}
+} 
 
 $(document).ready(function () {
-    dataLayer = L.geoJson(null).addTo(map);
-    //dataLayer.addTo(map);
-    //addTileLayer(map);
-    addDataToMap(dataLayer, 'lottype');
+    console.log('is it adding oddlots layer?');
     $('.choice').change(function () {
-        addDataToMap(dataLayer, $(this).val());
-        ///addDataToMap( $(this).val());
-        // not sure if this function should include the other addToMap calls
-        //dataLayer.addTo(map);
+        Choice(dataLayer, $(this).val());//Choice);
+    })
+    .change(); 
+})
 
-    });
-            map.fitBounds(dataLayer.getBounds());
-}); 
-//NYBBgeoJSON.addTo(map);
-//oddLotsGeoJSON.addTo(map);
+
+
+
+
+
+
 
 
